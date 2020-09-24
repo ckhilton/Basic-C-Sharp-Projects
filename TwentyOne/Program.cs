c510455ede824d36483a67c5640708b45bcfac1c
ckhilton <ckirkhilton@gmail.com>
2020-09-23 22:17:51 -0600

    Step 216 Assignment

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -8,50 +8,119 @@ namespace TwentyOne
 {
     class Program
     {
-        //***RULES TO A METHOD/FUNCTION: ACCESS MODIFIER, RETURN TYPE 'voic', A NAME 'Main', PARAMETERS '(string[] args), METHODS ALSO HAVE TO BE PART OF A CLASS. IF...
-        //...A METHOD IS BEING USED WITHOUT FIRST CREATING AN OBJECT OF THAT CLASS THEN IT HAS TO BE MARKED AS 'static'
         static void Main(string[] args) //***NOTE: AN ACCESS MODIFIER EXAMPLE IS 'public', WHICH WOULD GIVE ACCESS TO THE FUNCTION FROM THE REST OF THE PROGRAM
         {
-            Deck deckNew = new Deck(); //INSTANTIATE AN OBJECT FROM THE CLASS 'Public Class Deck' AND NAMED IT 'deckNew' (EMPTY/NO VALUES ASSIGNED YET)
-            deckNew = Shuffle(deckNew);
+            //============================= STEP 216 ASSIGNMENT =============================
+            Console.Write("\n===================================== STEP 216 ASSIGNMENT ===========================================\n" +
+                "\n  THIS CONSOLE APP WILL APPROXIMATE YOUR ANNUAL SALARY INFO BASED ON A 40 HR WORK WEEK\n");
+            //CONTROL VARIABLE
+            bool executing = true;
 
-            foreach (Card card in deckNew.Cards)
+            //ENTER DO-WHILE LOOP (FOR EASE OF TESTING PROGRAM)
+            do
             {
-                Console.WriteLine(card.Face + " of " + card.Suit);
+                //THE CODE I WANT TO WORK
+                try
+                {
+                    Console.Write("\n=========================================== START ===================================================\n" +                       
+                        "\n\tENTER AN HOURLY WAGE: ");
+                    //Console.WriteLine("\n\tENTER AN HOURLY WAGE: ");
+                    //GET USER INPUT
+                    int hourly = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("\n\t----------------------------------------------------");
+                    //GIVE USER INSTRUCTIONS
+                    Console.WriteLine("\n\tBASED ON AN HOURLY WAGE OF " + String.Format("{0:C}", hourly) + " PER HOUR ..." +
+                        "\n\n\tYOUR ANNUAL SALARY INFORMATION IS AS FOLLOWS ...");
+                    //CALL EACH FUNCTION
+                    int gross = Salary.Gross(hourly);
+                    Console.WriteLine("\n\t  - GROSS ANNUAL INCOME:\t" + String.Format("{0:C}", gross));
+
+                    int tax = Salary.Tax(gross);
+                    Console.WriteLine("\n\t  - AVERAGE TOTAL TAX:\t\t" + String.Format("{0:C}", tax) + " ***");
+
+                    int net = Salary.Net(gross);
+                    Console.WriteLine("\n\t  - RAW NET ANNUAL INCOME:\t" + String.Format("{0:C}", net));
+
+                    //ADDITIONAL INFO FOR USER
+                    Console.WriteLine("\n\tHERE IS SOME ADDITIONAL HELPFUL INFORMATION ..." +
+                        "\n\n\t\t(PRESS ENTER)");
+                    Console.ReadLine();
+
+                    //CALL TITHING/CHARITY FUNCTIONS
+                    int titheGross = Salary.Tithe_Gross(gross);
+                    int titheNet = Salary.Tithe_Net(net);
+                    int netTitheGross = Salary.Net_Tithe_Gross(net, titheGross);
+                    int netTitheNet = Salary.Net_Tithe_Net(net, titheNet);
+
+                    //DISPLAY INFORMATION/OUTPUT
+                    Console.WriteLine("\tA GENEROUS RELIGIOUS TITHE OR CHARITY DONATION OF 10% ..." +
+                        "\n\n\t  - ON YOUR GROSS INCOME IS:\t" + String.Format("{0:C}", titheGross) +
+                        "\n\n\t  - ON YOUR NET INCOME IS:\t" + String.Format("{0:C}", titheNet) +
+                        "\n\n\t\t(PRESS ENTER)");
+                    Console.ReadLine();
+
+                    Console.WriteLine("\t----------------------------------------------------------------------" +
+                        "\n\n\tHERE ARE YOUR NET ANNUAL INCOME VARIANCE TOTALS: " +                       
+                        "\n\n\tAFTER TAXES AND 10% TITHE OF GROSS:\t( NET TOTAL ) " + String.Format("{0:C}", netTitheGross) +
+                        "\n\n\tAFTER TAXES AND 10% TITHE OF NET:\t( NET TOTAL ) " + String.Format("{0:C}", netTitheNet) +
+                        "\n\n\tAFTER TAXES WITHOUT ANY DONATIONS:\t( NET TOTAL ) " + String.Format("{0:C}", net) +
+                        "\n\n\t*********************************************************************************" +
+                        "\n\tDISCLAIMER: THIS APP DOES NOT TAKE TAX BREAKS INTO CONSIDERATION. BUT YOU SHOULD!" +
+                        "\n\t*********************************************************************************" +                                  
+                        "\n\n\t(PRESS ENTER)");
+                    Console.ReadLine();
+                }
+
+                //================ EXCEPTION HANDLING ================              
+                //WRONG FORMAT CATCH
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("\n\tTISK TISK, WRONG FORMAT!!! " +
+                        "\n\n\t   - TEXT NOT ALLOWED. " +
+                        "\n\n\t   - WHOLE NUMBERS ONLY. " +
+                        "\n\n\t   - NULL IS NOT ACCEPTABLE EITHER." +
+                        "\n\n\t(PRESS ENTER TO LEAVE THE TRY-CATCH BLOCK)");
+                    Console.ReadLine();
+                }
+                //GENERAL CATCH IF TOO LARGE OF OR TOO SMALL OF AN INTEGER WERE ENTERED
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n\tEASY DOES IT THERE!!!" +
+                        "\n\n\t   - YOUR " + ex.Message.ToUpper() +
+                        "\n\n\t   - NOT TOO BIG." +
+                        "\n\n\t   - NOT TOO NEGATIVE." +
+                        "\n\n\t   - NICE AND EASY." +
+                        "\n\n\t(PRESS ENTER TO LEAVE THE TRY-CATCH BLOCK)");
+                    Console.ReadLine();
+                }
+                //INSTRUCTIONS TO USER
+                finally
+                {
+                    //PROGRAM NOTE   
+                    Console.Write("======================================= END TRY/CATCH ===============================================\n" +
+                        "\n\t             ---- (PRESS ENTER TO TRY ANOTHER HOURLY WAGE) ----\n\n" +
+                        "\t\t\t\t            OR\n\n" +
+                        "\t\t        XXX (PRESS \'X\' TO SEE REFERENCE/SOURCE) XX");
+                        
+                    string exit = Console.ReadLine();
+                    //OPTION TO EXIT WITH CONTROLLED CASE SENSITIVITY
+                    if ((exit == "x") || (exit == "X"))
+                    {
+                        executing = false;
+                    }
+                }
             }
+            while (executing);
 
-            //Console.WriteLine(deckNew.Cards[0].Face + " of " + deckNew.Cards[0].Suit);
-            Console.WriteLine("TOTAL COUNT OF CARDS CREATED: " + deckNew.Cards.Count);
-            Console.ReadLine();
+            //DISCLAIMER AND REFERENCES FOR USER
+            Console.WriteLine("\n=====================================================================================================");
 
+            Console.WriteLine("\nREFERENCE: " +
+                "\n\n*** THE AVERAGE SINGLE AMERICAN GAVE 29.6% TO 3 TAXES IN 2018: INCOME, MEDICARE, AND SOCIAL SECURITY.\n" +
+                "\nSOURCE: " +
+                "\n\nhttps://www.thebalance.com/what-the-average-american-pays-in-taxes-4768594#:~:text=The%20average%20income%20tax%20rate,one%20study%20of%20BLS%20numbers.");
+            Console.WriteLine("\n(PRESS ENTER TO CLOSE WINDOW)\n");
+            Console.ReadKey(true);
         }
-
-        //SHUFFLE THE DECK
-        public static Deck Shuffle(Deck deck)
-        {
-            List<Card> TempList = new List<Card>();
-            Random random = new Random();
-
-            while (deck.Cards.Count > 0)
-            {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                TempList.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex);
-            }
-            deck.Cards = TempList;
-            return deck;
-        }
-    }
+    }        
 }
-
-
-
-//================= OLD/SLOWER VERSION OF TYPING OUT CODE THAT WAS UPGRADED BY THE MORE EFFICIENT CODE THAT'S USED ABOVE =============
-
-//deckNew.Cards = new List<Card>(); //MAKE THE PROPERTY 'Cards' INTO A BLANK LIST. REMEMBER THE CLASS "Public Deck" HAS ONLY 1 PROPERTY (WHICH IS A DATA TYPE OF LIST, CALLED 'Public List<Card> Cards')
-
-//======== ...AND ALL OF THIS CODE WAS MOVED INTO THE CLASS 'Decks' FOR EASE OF CREATING NEW DECKS===============
-//Card cardOne = new Card();
-//cardOne.Face = "Queen";
-//cardOne.Suit = "Spades";
-//deckNew.Cards.Add(cardOne);
---
+++ TwentyOne/Salary.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Salary
+    {
+        //SET ATTRIBUTES TO PUBLIC AND ALLOW GET:SET
+        public int hourly { get; set; }
+        public int gross { get; set; }
+        public int tax { get; set; }
+        public int net { get; set; }
+        public int tithing { get; set; }
+        public int newNet { get; set; }
+
+        //SIMPLE MATH METHODS/FUNCTIONS
+        public static int Gross(int hourly)
+        {
+            int gross = hourly * 2080;
+            return gross;
+        }
+      
+        public static int Tax(int gross)
+        {
+            int tax = gross * 30 / 100;
+            return tax;
+        }
+
+        public static int Net(int gross)
+        {
+            int net = gross - Tax(gross);
+            return net;
+        }
+
+        public static int Tithe_Gross(int gross)
+        {
+            int titheGross = gross * 10 / 100;
+            return titheGross;
+        }
+
+        public static int Tithe_Net(int net)
+        {
+            int titheNet = net * 10 / 100;
+            return titheNet;
+        }
+
+        public static int Net_Tithe_Gross(int net, int titheGross)
+        {
+            int netTitheGross = net - titheGross;
+            return netTitheGross;
+        }
+
+        public static int Net_Tithe_Net(int net, int titheNet)
+        {
+            int netTitheNet = net - titheNet;
+            return netTitheNet;
+        }        
+    }    
+}
