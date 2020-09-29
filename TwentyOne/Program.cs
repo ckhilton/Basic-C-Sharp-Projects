69807f87d8c3375d379ad9739675b2db7e36b619
ckhilton <ckirkhilton@gmail.com>
2020-09-29 02:26:21 -0600

    Polymorphism

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,6 +10,12 @@ namespace TwentyOne
     {
         static void Main(string[] args) 
         {
+            //CREATE LIST
+            List<Game> games = new List<Game>();
+            TwentyOneGame game = new TwentyOneGame();
+            //ADD A 'game' TO THE LIST 'games' WHICH WILL USE POLYMORPHISM 
+            games.Add(game);
+
             Deck deck = new Deck();
             deck.Shuffle(3);
 
@@ -17,13 +23,15 @@ namespace TwentyOne
             {
                 Console.WriteLine(card.Face.ToUpper() + " OF " + card.Suit.ToUpper());
             }
-            Console.WriteLine(deck.Cards)
-            //INSTANTIATE 'TwentyOneGame' OBJECT FROM THE SUPERCLASS 'Game' (aka 'Game' CLASS IS INHERITED BY 'TwentyOneGame')
-            TwentyOneGame game = new TwentyOneGame();            
-            game.Players = new List<string>() { "Kirk", "Bill", "Joe" };
-            game.ListPlayers();
-            game.Play(); //THIS IS THE ONLY METHOD OR PROPERTY THAT IS SPECIFIC TO THE 'TwentyOneGame' CLASS (NOT ACCESSIBLE TO THE 'Game' METHOD)
+            Console.WriteLine("\n\tTOTAL COUNT OF CARDS CREATED: " + deck.Cards.Count);
             Console.ReadLine();
+
+            ////INSTANTIATE 'TwentyOneGame' OBJECT FROM THE SUPERCLASS 'Game' (aka 'Game' CLASS IS INHERITED BY 'TwentyOneGame')
+            //TwentyOneGame game = new TwentyOneGame();            
+            //game.Players = new List<string>() { "Kirk", "Bill", "Joe" };
+            //game.ListPlayers();
+            //game.Play(); //THIS IS THE ONLY METHOD OR PROPERTY THAT IS SPECIFIC TO THE 'TwentyOneGame' CLASS (NOT ACCESSIBLE TO THE 'Game' METHOD)
+            //Console.ReadLine();
             
         }
     }
