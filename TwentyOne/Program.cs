be2be055efdfc35647f63f6adc65bcc6069100bf
ckhilton <ckirkhilton@gmail.com>
2020-09-29 01:01:20 -0600

    Inheritance

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,17 +10,30 @@ namespace TwentyOne
     {
         static void Main(string[] args) 
         {
-            //INSTANTIATE OBJECT FROM CLASS CONTSTRUCTOR 'Deck()'
-            Deck deck = new Deck();
-            deck.Shuffle(3);
+            //INSTANTIATE OBJECT FROM CLASS CONSTRUCTOR 'Game()'
+            //Game game = new Game();
+            //game.Dealer = "Kirk";
+            //game.Name = "TwentyOne";
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face.ToUpper() + " OF " + card.Suit.ToUpper());                
-            }
-
-            Console.WriteLine("\n\tTOTAL COUNT OF CARDS CREATED: " + deck.Cards.Count);
+            //INSTANTIATE 'TwentyOneGame' OBJECT FROM THE SUPERCLASS 'Game' (aka 'Game' CLASS IS INHERITED BY 'TwentyOneGame')
+            //TwentyOneGame game = new TwentyOneGame();
+            Game game = new Game();
+            game.Players = new List<string>() { "Kirk", "Bill", "Joe" };
+            game.ListPlayers();
+            game.Play(); //THIS IS THE ONLY METHOD OR PROPERTY THAT IS SPECIFIC TO THE 'TwentyOneGame' CLASS (NOT ACCESSIBLE TO THE 'Game' METHOD 
             Console.ReadLine();
+            
+            //INSTANTIATE OBJECT FROM CLASS CONTSTRUCTOR 'Deck()'
+            //Deck deck = new Deck();
+            //deck.Shuffle(3);
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face.ToUpper() + " OF " + card.Suit.ToUpper());                
+            //}
+
+            //Console.WriteLine("\n\tTOTAL COUNT OF CARDS CREATED: " + deck.Cards.Count);
+            //Console.ReadLine();
         }
     }
 }
