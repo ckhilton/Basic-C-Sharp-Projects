d5d720dd1ab530e0a2d97d980d4f52e8fa3adb43
ckhilton <ckirkhilton@gmail.com>
2020-09-29 04:11:30 -0600

    Interfaces

---
+++ TwentyOne/IWalkAway.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    interface IWalkAway
+    {
+        void WalkAway(Player player); //EVERYTHING IS AUTOMATICALLY PUBLIC IN AN INTERFACE
+    }
+}
+
+//*INTERFACE RULES:
+//      1) NAMING CONVENTION ALWAYS STARTS WITH THE CAPITAL LETTER 'I'
+//      2) EVERYTHING IS AUTOMATICALLY PUBLIC
+//      3) CLASSES CAN INHERIT AS MANY INTERFACES AS DESIRED
+//      4) INTERFACES ARE SIMILAR TO ABSTRACT CLASSES IN THAT THERE ARE NO IMPLEMENTATION DETAILS
+
