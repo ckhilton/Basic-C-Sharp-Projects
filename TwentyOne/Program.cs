100800822e3376bda789425f6995f493444c38d0
ckhilton <ckirkhilton@gmail.com>
2020-09-29 02:01:26 -0600

    Push

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,7 +10,14 @@ namespace TwentyOne
     {
         static void Main(string[] args) 
         {
-           
+            Deck deck = new Deck();
+            deck.Shuffle(3);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face.ToUpper() + " OF " + card.Suit.ToUpper());
+            }
+            Console.WriteLine(deck.Cards)
             //INSTANTIATE 'TwentyOneGame' OBJECT FROM THE SUPERCLASS 'Game' (aka 'Game' CLASS IS INHERITED BY 'TwentyOneGame')
             TwentyOneGame game = new TwentyOneGame();            
             game.Players = new List<string>() { "Kirk", "Bill", "Joe" };
