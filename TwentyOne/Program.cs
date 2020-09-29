c561b60fd3d595280203cee466c369910213784d
ckhilton <ckirkhilton@gmail.com>
2020-09-29 10:45:21 -0600

    Push

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -19,3 +19,29 @@ namespace TwentyOne
         }
     }
 }
+
+
+//namespace TwentyOne
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Game game = new TwentyOneGame();
+//            game.Players = new List<Player>();
+//            Player player = new Player();
+//            player.Name = "Kirk";
+//            game += player; //SAME THING AS game = game + player;
+//            game -= player; //SAME THING AS game = game - player;
+//            Deck deck = new Deck();
+//            deck.Shuffle(3);
+
+//            foreach (Card card in deck.Cards)
+//            {
+//                Console.WriteLine(card.Face.ToUpper() + " OF " + card.Suit.ToUpper());
+//            }
+//            Console.WriteLine("\n\tTOTAL COUNT OF CARDS CREATED: " + deck.Cards.Count);
+//            Console.ReadLine();
+//        }
+//    }
+//}
