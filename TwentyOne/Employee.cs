b9ab7db4659928dfd1014866b906f35a870860b6
ckhilton <ckirkhilton@gmail.com>
2020-09-29 01:39:50 -0600

    Step 229 Assignment

---
+++ TwentyOne/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    //CREATE CLASS 'Employee' THAT INHERITS SUPERCLASS 'Person' 
+    public class Employee : Person
+    {
+        //CREATE PUBLIC PROPERTY 'Id' AS TYPE INTEGER
+        public int Id { get; set; }
+    }
+}
---
+++ TwentyOne/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Person
+    {       
+        //PUBLIC STRING PROPERTIES 'FirstName' AND 'LastName'
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //PUBLIC VOID METHOD WITH NO INPUTS
+        public void SayName()
+        {
+            //DISPLAY PERSONS FULL NAME TO THE CONSOLE
+            Console.WriteLine("Name: " + FirstName + LastName);
+        }
+    }
+}
