40a8beb98db9fdc880a85023ec07cdfafa7d6004
ckhilton <ckirkhilton@gmail.com>
2020-10-01 15:41:52 -0600

    Step 245 Assignment

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,56 +10,117 @@ namespace TwentyOne
     {
         static void Main(string[] args) 
         {
-            //USER INSTRUCTIONS 
-            Console.WriteLine("\tWELCOME, YOU ARE NOW ATTEMPTING TO ENTER ONLINE THE EMPLOYEE PORTAL. " +
-                "\n\n\tPLEASE FILL OUT THE FOLLOWING:");
-            //GET USER INPUT FOR FIRST NAME 
-            Console.Write("\n" +
-                "\tFIRST NAME: ");
-            string first = Console.ReadLine();
+            //============================= STEP 245 ASSIGNMENT =============================
+            Console.Write("===================================== STEP 245 ASSIGNMENT =====================================\n");
 
-            ////GET USER INPUT FOR LAST NAME
-            Console.Write("\n" +
-                "\tLAST NAME: ");
-            string last1 = Console.ReadLine();
+            //CONTROL VARIABLE
+            bool executing = true;
 
-            //GET USER EMPLOYEE ID
-            Console.Write("\n" +
-                "\tEMPLOYEE ID: ");
-            int Id1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("\n\t===================================================================================" +
-                "\n\n\tFOR VERIFICATION PURPOSES, PLEASE ENTER YOUR EMPLOYEE ID AGAIN: ");
-
-            //GET USER EMPLOYEE ID 
-            Console.Write("\n" +
-                "\tEMPLOYEE ID: ");
-            int Id2 = Convert.ToInt32(Console.ReadLine());
-
-
-
-            //INSTANTIATE AND INITIATE 'Employee' OBJECT WITH ASSIGNED 'FirstName' AND 'LastName' VALUES
-            Employee employee1 = new Employee();
-            Employee employee2 = new Employee();
-
-            employee1.Id = Id1;
-            employee2.Id = Id2;
-
-            //CALL SUPERCLASS METHOD 'FullName' ON THE 'Employee' OBJECT
-            employee1.employeeInfo(Id: Id1, FirstName: first, Info: out string Full1, LastName: last1);
-            employee2.employeeInfo(Id: Id2, FirstName: first, Info: out string Full2, LastName: last1);
-
-            //CONDITION TO BE CHECKED BASED UPON EMPLOYEE ID 
-            if (employee1 == employee2)
+            //ENTER DO-WHILE LOOP (FOR EASE OF TESTING PROGRAM)
+            do
             {
-                Console.WriteLine("\n\tYOUR EMPLOYEE ID HAS BEEN VALIDATED!");
+                //THE CODE I WANT TO WORK
+                try
+                {
+                   
+                    //USER INSTRUCTIONS 
+                    Console.WriteLine("\n\tWELCOME, YOU ARE NOW ATTEMPTING TO ENTER ONLINE THE EMPLOYEE PORTAL. " +
+                        "\n\n============================================ START ============================================" +
+                        "\n\n\tPLEASE FILL OUT THE FOLLOWING:");
+                    //GET USER INPUT FOR FIRST NAME 
+                    Console.Write("\n" +
+                        "\tFIRST NAME: ");
+                    string first = Console.ReadLine();
+
+                    ////GET USER INPUT FOR LAST NAME
+                    Console.Write("\n" +
+                        "\tLAST NAME: ");
+                    string last1 = Console.ReadLine();
+
+                    //GET USER EMPLOYEE ID
+                    Console.Write("\n" +
+                        "\tEMPLOYEE ID: ");
+                    int Id1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("\n\t=======================================================================================" +
+                        "\n\n\tFOR VERIFICATION PURPOSES, PLEASE ENTER YOUR EMPLOYEE ID AGAIN: ");
+
+                    //GET USER EMPLOYEE ID 
+                    Console.Write("\n" +
+                        "\tEMPLOYEE ID: ");
+                    int Id2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+                    //INSTANTIATE AND INITIATE 'Employee' OBJECT WITH ASSIGNED 'FirstName' AND 'LastName' VALUES
+                    Employee employee1 = new Employee();
+                    Employee employee2 = new Employee();
+
+                    employee1.Id = Id1;
+                    employee2.Id = Id2;
+
+                    //CALL SUPERCLASS METHOD 'FullName' ON THE 'Employee' OBJECT
+                    employee1.employeeInfo(Id: Id1, FirstName: first, Info: out string Full1, LastName: last1);
+                    employee2.employeeInfo(Id: Id2, FirstName: first, Info: out string Full2, LastName: last1);
+
+                    //CONDITION TO BE CHECKED BASED UPON EMPLOYEE ID 
+                    if (employee1 == employee2)
+                    {
+                        Console.WriteLine("\n\tYOUR EMPLOYEE ID HAS BEEN VALIDATED!\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n\tEMPLOYEE ID: " + Id2 + " DID NOT MATCH!" +
+                            "\n\n\tPLEASE CHECK YOUR EMPLOYEE ID CAREFULLY AND TRY AGAIN\n");
+                    }
+                }
+
+                //================ EXCEPTION HANDLING ================              
+                //WRONG FORMAT CATCH
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("\n\tTISK TISK, WRONG FORMAT!!! " +
+                        "\n\n\t   - TEXT NOT ALLOWED. " +
+                        "\n\n\t   - WHOLE NUMBERS ONLY. " +
+                        "\n\n\t   - NULL IS NOT ACCEPTABLE EITHER." +
+                        "\n\n\t(PRESS ENTER TO LEAVE THE TRY-CATCH BLOCK)");
+                    Console.ReadLine();
+                }
+                //GENERAL CATCH IF TOO LARGE OF OR TOO SMALL OF AN INTEGER WERE ENTERED
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n\tEASY DOES IT THERE!!!" +
+                        "\n\n\t   - YOUR " + ex.Message.ToUpper() +
+                        "\n\n\t   - NOT TOO BIG." +
+                        "\n\n\t   - NOT TOO NEGATIVE." +
+                        "\n\n\t   - NICE AND EASY." +
+                        "\n\n\t(PRESS ENTER TO LEAVE THE TRY-CATCH BLOCK)");
+                    Console.ReadLine();
+                }
+                //INSTRUCTIONS TO USER
+                finally
+                {
+                    //PROGRAM NOTE   
+                    Console.Write("======================================== END TRY/CATCH ========================================\n" +
+                        "\n\t                 ---- (PRESS ENTER TO RUN AGAIN) ----\n\n" +
+                        "\t\t\t\t            OR\n\n" +
+                        "\t\t        XXX (PRESS \'X\' TO EXIT THE PROGRAM) XX");
+
+                    string exit = Console.ReadLine();
+                    Console.WriteLine("\n===============================================================================================");
+                    //OPTION TO EXIT WITH CONTROLLED CASE SENSITIVITY
+                    if ((exit == "x") || (exit == "X"))
+                    {
+                        executing = false;
+                    }
+                }
             }
-            else
-            {
-                Console.WriteLine("\n\tEMPLOYEE ID: " + Id2 + " BELONGS TO A DIFFERENT EMPLOYEE" +
-                    "\n\n\tPLEASE CHECK YOUR EMPLOYEE ID CAREFULLY AND TRY AGAIN.");
-            }        
-            Console.ReadLine();          
+            while (executing);
+
+            //DISCLAIMER AND REFERENCES FOR USER
+            Console.WriteLine("\n\tTHANK YOU FOR USING THE \"STEP 245 ASSIGNMENT\" PROGRAM" +
+                "\n\n\t(PRESS ENTER TO CLOSE THE WINDOW)\n");
+            Console.ReadKey(true);
         }
     }
 }
