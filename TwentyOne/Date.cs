eb1408f3279cecae0d6d54c4026e19522fbeeca2
ckhilton <ckirkhilton@gmail.com>
2020-10-01 22:56:44 -0600

    Push

---
+++ TwentyOne/Date.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Date
+    {  
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }       
+    }
+}
--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,35 +10,33 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n\tPLEASE ENTER THE CURRENT DAY OF THE WEEK: \n\n\t");
+
+            string weekday = Console.ReadLine();
+
+            Date.DaysOfTheWeek weekday = new Date.DaysOfTheWeek();
+
+
+            weekday = Enum.Parse(DaysOfTheWeek);
+
+
+
             ////USING THE ENUM FROM 'Card' CLASS
             //Card card = new Card();
             //card.Suit = Suit.Clubs;
             //int underlyingValue = (int)Suit.Diamonds; //CASTING THE ENUM TO IT'S UNDERLYING DATA TYPE (INT). THIS COULD HAVE ALSO BEEN WRITTEN OUT AS 'int underlyingValue = Convert.ToInt32(underlyingValue)'
             //Console.WriteLine(underlyingValue);
 
-            Deck deck = new Deck();
-            deck.Shuffle(3);
+            //Deck deck = new Deck();
+            //deck.Shuffle(3);
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " OF " + card.Suit);
-            }
-            Console.WriteLine("\n\tTOTAL COUNT OF CARDS CREATED: " + deck.Cards.Count);
-            Console.ReadLine();
-        }
-
-        //DaysofTheWeek day = DaysofTheWeek.Monday;
-
-        //public enum DaysofTheWeek
-        //{
-        //    Monday,
-        //    Tuesday, 
-        //    Wednesday,
-        //    Thursday,
-        //    Friday,
-        //    Saturday, 
-        //    Sunday
-        //}
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " OF " + card.Suit);
+            //}
+            //Console.WriteLine("\n\tTOTAL COUNT OF CARDS CREATED: " + deck.Cards.Count);
+            //Console.ReadLine();
+        }       
     }
 }
 
