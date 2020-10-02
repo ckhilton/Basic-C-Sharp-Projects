4e2e465b1eb74354b4eed4d701142854bc0ea926
ckhilton <ckirkhilton@gmail.com>
2020-10-02 16:03:10 -0600

    Step 254 Assignment

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,46 +10,77 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            //============================= STEP 254 ASSIGNMENT =============================
+            Console.Write("===================================== STEP 254 ASSIGNMENT =====================================\n");
+
+            //LOOP CONTROL VARIABLE
             bool run = true;
             do
             {
                 try
                 {
+                    //USER INSTRUCTIONS 
+                    Console.Write("\n============================================ START ============================================" +
+                        "\n\n\tPLEASE ENTER THE CURRENT DAY OF THE WEEK: ");
 
-                    //GET USER INPUT FOR FIRST NAME 
-                    Console.Write("\n" +
-                        "\n\tPLEASE ENTER THE CURRENT DAY OF THE WEEK: ");
+                    //ESTABLISH ACTUAL DAY OF WEEK
+                    DayOfWeek weekday = DateTime.Today.DayOfWeek;
+                    
+                    //GET USER INPUT 
                     string input = Console.ReadLine();
 
-                    Day today;
+                    //PARSE THROUGH THE ENUM 'Day' IN SEARCH OF MATCHING USER INPUT
+                    Day today = (Day)Enum.Parse(typeof(Day), input.ToUpper());
 
-                    today = (Day)Enum.Parse(typeof(Day), input);
-                    Console.WriteLine("\n\tTODAY IS: " + input.ToUpper());
+                    //CONDITIONS
+                    // 1) IF THE INPUT IS VALID AND TODAY IS FRIDAY...
+                    if (input.ToUpper() == "FRIDAY")
+                    {
+                        // DISPLAY THE FOLLOWING
+                        Console.WriteLine("\n\tFRIDAY!!! ************ T.G.I.F. ************ FRIDAY!!!");
+                    }
+                    // 2) IF THE INPUT IS VALID BUT NOT THE CORRECT DAY OF THE WEEK
+                    else if (input.ToUpper() != weekday.ToString().ToUpper())
+                    {
+                        // SHOW USER IT'S NOT THE CORRECT DAY OF THE WEEK
+                        Console.WriteLine("\n\tTODAY IS NOT " + input.ToUpper());
+                    }
+                    // 3) IF IT'S NOT FRIDAY AND THE INPUT IS A VALID ENUM PARAMETER
+                    else
+                    {
+                        // DISPLAY THE FACT ABOUT TODAY
+                        Console.WriteLine("\n\tTODAY IS: " + input.ToUpper());
+                    }
                 }
+
+                // EXCEPTION HANDLING
+                // THE INPUT DOES NOT MATCH ANY VALUES INSIDE THE DATA TYPE 'Day'
                 catch (Exception ex)
                 {
-                    Console.WriteLine("\n\tPLEASE ENTER AN ACTUAL DAY OF THE WEEK");
+                    //NOTIFY USER OF THE EXCEPTION
+                    Console.WriteLine("\n\t***PLEASE ENTER AN ACTUAL DAY OF THE WEEK***");
                     Console.WriteLine("\n\t" + ex.Message.ToUpper());
                 }
                 finally
                 {
-                    Console.WriteLine("\n\tYOU HAVE EXITED THE TRY-CATCH LOOP");
-                    Console.Write("\n" +
-                        "\t\n\tPRESS ENTER ENTER TO RUN THE PROGRAM AGAIN" +
-                        "\n\n\t\t OR" +
+                    Console.Write("\n======================================= END TRY / CATCH =======================================" +
+                        "\n\n\tPRESS ENTER TO RUN THE PROGRAM AGAIN" +
+                        "\n\n\t\t\t OR" +
                         "\n\n\tPRESS 'X' TO EXIT THE PROGRAM: ");
                     string stop = Console.ReadLine();
                     if (stop.ToUpper() == "X")
                     {
                         run = false;
-                        Console.WriteLine("\n=======================" +
-                            "\n\n\tPRESS ENTER");
+                        Console.WriteLine("\n=====================================================================================================");
                     }
                 }
             }
             while (run);
-            Console.ReadLine();
+
+            //EXIT PROGRAM INFO
+            Console.WriteLine("\n\tTHANK YOU FOR USING THE \"STEP 254 ASSIGNMENT\" PROGRAM!" +
+                "\n\n\t(PRESS ENTER TO CLOSE THE WINDOW)\n");
+            Console.ReadKey(true);
         }
     }
 }
-
