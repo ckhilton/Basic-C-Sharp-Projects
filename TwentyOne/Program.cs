355545de4db428651a4c8fa8c581b1a363b89200
ckhilton <ckirkhilton@gmail.com>
2020-10-12 13:44:25 -0600

    Step_290_Assignment

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -14,67 +14,16 @@ namespace TwentyOne
             Console.WriteLine("WELCOME TO THE GRAND HOTEL AND CASINO. LET'S START BY TELLING ME YOUR NAME.");
             string playerName = Console.ReadLine().ToUpper();
             Console.WriteLine("HELLO {0}! HOW MUCH MONEY DID YOU BRING TODAY?", playerName);
-            int bank = Convert.ToInt32(Console.ReadLine());
-            //APPEND FILE WITH AMOUNT OF MONEY EACH USER SAYS THEY BROUGHT TODAY
-            using (StreamWriter file = new StreamWriter(@"C:\Users\Student\Desktop\Basic_C#_Programs - Copy\TwentyOne\TwentyOne\MoneyLog.txt", true))
-            {
-                file.WriteLine(bank);
-            }
-            
-            Console.Write("PLEASE CONFIRM THAT YOU BROUGH ")
+            string playerBank = Console.ReadLine();
 
+            //WRITE THE 'playerBank' AMOUNT TO THE FILE 'PlayerLog.txt'
+            File.WriteAllText(@"C:\Users\Student\Desktop\Basic_C#_Programs - Copy\TwentyOne\TwentyOne\ZZ_PlayerLog.txt", playerBank);
+            
+            //READS ALL TEXT FROM 'PlayerLog.txt' AND STORES IT IN A STRING VARIABLE CALLED 'text'
+            string text = File.ReadAllText(@"C:\Users\Student\Desktop\Basic_C#_Programs - Copy\TwentyOne\TwentyOne\ZZ_PlayerLog.txt");
+            Console.WriteLine("IT LOOKS LIKE YOU'VE BROUGHT ${0} TODAY! LET'S PLAY SOME 21!", text);
 
-                string bank = string.Format(System.Stream.ReadAllText(@"C:\Users\Student\Desktop\Basic_C#_Programs - Copy\TwentyOne\TwentyOne\Log.txt", true))
-            int bank = Convert.ToInt32(Console.ReadLine());
-            
-            
-            
-            Console.WriteLine("HELLO, {0}. WOULD YOU LIKE TO JOIN A GAME OF 21 RIGHT NOW?", playerName);
-            string answer = Console.ReadLine().ToUpper();
-            if (answer == "YES" || answer == "YEAH" || answer == "Y" || answer == "YA" || answer == "YEA" || answer == "YEP" || answer == "YUP")
-            {
-                Player player = new Player(name: playerName, beginningBalance: bank);
-                Game game = new TwentyOneGame(); //USES POLYMORPHISM
-                game += player;
-                player.IsActivelyPlaying = true;
-                while (player.IsActivelyPlaying && player.Balance > 0) //DOES PLAYER WANT TO KEEP PLAYING AND DO THEY HAVE ENOUGH MONEY TO PLAY
-                {
-                    
-                    if (player.Balance <= 0)
-                    {
-                        if (player.IsBroke() == false)
-                        {
-                            player.IsActivelyPlaying = true;
-                        }
-                        else 
-                        { 
-                            player.IsActivelyPlaying = false; 
-                        }
-                    }
-                    game.Play();
-                }
-                //game -= player;
-                Console.WriteLine("THANK YOU FOR PLAYING 21!");
-            }
-            Console.WriteLine("FEEL FREE TO LOOK AROUND THE CASINO. BYE FOR NOW.");
-            Console.Read();
+            Console.ReadLine();
         }
     }
 }
-
-//Game game = new TwentyOneGame();
-//game.Players = new List<Player>();
-//Player player = new Player();
-//player.Name = "Kirk";
-//game = game + player;
-//game = game - player;
-
-//Deck deck = new Deck();
-//deck.Shuffle(3);
-
-//foreach (Card card in deck.Cards)
-//{
-//    Console.WriteLine(card.Face.ToUpper() + " OF " + card.Suit.ToUpper());
-//}
-//Console.WriteLine("\n\tTOTAL COUNT OF CARDS CREATED: " + deck.Cards.Count);
-//Console.ReadLine();
