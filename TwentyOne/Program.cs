e9e77f57d4c0c08413dccbf0df70d1fc69f354da
ckhilton <ckirkhilton@gmail.com>
2020-10-14 01:13:17 -0600

    Step_303_Assignment

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -52,15 +52,19 @@ namespace TwentyOne
                 {
                     //CALL 'Verify' METHOD
                     employee.Verify();
-                    //INSTANTIATE NEW OBJECT OF CLASS 'StreamWriter'
-                    using (StreamWriter file = new StreamWriter(@"C:\Users\Student\Desktop\Logs\EmployeeLog.txt", true)) ; //'true' ALLOWS 'file' TO BE APPENDED INSTEAD OF OVERWRITTEN
-                    //INSTANTIATE NEW GUID (GLOBAL UNIQUE IDENTIFIER) WITH THE 'Employee' PROPERTY OF 'EmpID'
-                    file.WriteLine(EmpID);
-                    //LOG ALL VALUES IN THE LIST PROPERTY 'Full' THAT ARE IN THE 'Employee' CLASS OBJECT 'employee' AND 
-                    foreach (string value in all)
-                    {                        
-                        file.WriteLine(value);                     
-                    }
+                    //INSTANTIATE NEW GUID (GLOBAL UNIQUE IDENTIFIER) WITH THE 'Player' PROPERTY OF 'ID'
+                    employee.EmpID = Guid.NewGuid();
+                    //LOG THE USERS NAME 
+                    using (StreamWriter file = new StreamWriter(@"C:\Users\Student\Desktop\Basic_C#_Programs - Copy\TwentyOne\TwentyOne\EmployeeLog.txt", true)) //'true' APPENDS THE LIST, RATHER THAN OVERWRITING IT EACH TIME.
+                    {
+                        file.Write("EMPLOYEE ID: {0}", employee.EmpID);
+                        //LOG ALL VALUES IN THE LIST PROPERTY 'Full' THAT ARE IN THE 'Employee' CLASS OBJECT 'employee' AND                       
+                        file.WriteLine("\nONLINE ACCESS ID: {0}" +
+                            "\nLAST NAME: {1}" +
+                            "\nFIRST NAME: {2}" +
+                            "\nDOB: {3}" +
+                            "\nDEPARTMENT: {4}", all.ElementAt(0), all.ElementAt(2), all.ElementAt(1), all.ElementAt(3), all.ElementAt(4));       
+                    }                    
                     Console.WriteLine("\nYOUR ONLINE EMPLOYEE SETUP IS COMPLETE!!!");
                     break;
                 }
@@ -79,76 +83,3 @@ namespace TwentyOne
         }
     }
 }
-
-//ADDITIONAL FEATURES OF C# VIDEO PART1
-//CONSTRUCTOR CALL LINES
-
-////Player newPlayer = new Player("Kirk"); //FROM CONSTRUCTOR CALL CHAIN (I.E. "CHAINING) USED IN THE 'Player' CLASS
-////SHORTENED VERSION OF THE SAME CODE USED BELOW WITH THE 'var' DATA TYPE
-//var newPlayer = new Player("Kirk");
-
-//Dictionary<string, string> newDictionary = new Dictionary<string, string>();
-////                      OR
-//var newDictionary = new Dictionary<string, string>();
-
-
-
-//Game game = new TwentyOneGame();
-//game.Players = new List<Player>();
-//Player player = new Player();
-//player.Name = "Kirk";
-//game = game + player;
-//game = game - player;
-
-//Deck deck = new Deck();
-//deck.Shuffle(3);
-
-//foreach (Card card in deck.Cards)
-//{
-//    Console.WriteLine(card.Face.ToUpper() + " OF " + card.Suit.ToUpper());
-//}
-//Console.WriteLine("\n\tTOTAL COUNT OF CARDS CREATED: " + deck.Cards.Count);
-//Console.ReadLine();
-
-
-
-//GLOBAL UNIQUE IDENTIFIER EXAMPLE
-//Guid identifier = Guid.NewGuid();
-
-//Console.WriteLine("WELCOME TO THE {0}. LET'S START BY TELLING ME YOUR NAME.", string.Format(casinoName));
-//            string playerName = Console.ReadLine().ToUpper();
-//Console.WriteLine("AND HOW MUCH MONEY DID YOU BRING TODAY?");
-//            int bank = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("HELLO, {0}. WOULD YOU LIKE TO JOIN A GAME OF 21 RIGHT NOW?", playerName);
-//            string answer = Console.ReadLine().ToUpper();
-//            if (answer == "YES" || answer == "YEAH" || answer == "Y" || answer == "YA" || answer == "YEA" || answer == "YEP" || answer == "YUP")
-//            {
-//                Player player = new Player(name: playerName, beginningBalance: bank) { ID = Guid.NewGuid() };
-//                using (StreamWriter file = new StreamWriter(@"C:\Users\Student\Desktop\Logs\TwentyOne_GameLo.txt", true))
-//                {
-//                    file.WriteLine(player.ID);
-//                }
-
-//                Game game = new TwentyOneGame(); //USES POLYMORPHISM
-//game += player;
-//                player.IsActivelyPlaying = true;
-//                while (player.IsActivelyPlaying && player.Balance > 0) //DOES PLAYER WANT TO KEEP PLAYING AND DO THEY HAVE ENOUGH MONEY TO PLAY
-//                {
-                    
-//                    if (player.Balance <= 0)
-//                    {
-//                        if (player.IsBroke() == false)
-//                        {
-//                            player.IsActivelyPlaying = true;
-//                        }
-//                        else 
-//                        { 
-//                            player.IsActivelyPlaying = false; 
-//                        }
-//                    }
-//                    game.Play();
-//                }
-//                //game -= player;
-//                Console.WriteLine("THANK YOU FOR PLAYING 21!");
-//            }
-//            Console.WriteLine("FEEL FREE TO LOOK AROUND THE CASINO. BYE FOR NOW.");
